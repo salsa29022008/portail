0d3a41ee260bfa4d9995476fc8d9b554c325090b
salsa29022008 <103181349+salsa29022008@users.noreply.github.com>
2022-06-16 11:02:36 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Yes)
+})
+basic.forever(function () {
+	
+})
